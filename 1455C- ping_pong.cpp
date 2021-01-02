badbfc0d25396b5581004c950a7bdfd28eed2de5
Saransh Jain <45128915+saransh37@users.noreply.github.com>
2021-01-03 00:28:44 +0530

    Add files via upload

---
+++ 1455C- ping_pong.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin>>t;
+   while(t--){
+    int a,b;
+    cin>>a>>b;
+    cout<<a-1<<" "<<b<<"\n";
+    }
+   }
+
+
